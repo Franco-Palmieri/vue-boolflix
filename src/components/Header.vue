d391708a38d789dcc837a431fed47ddc2906a725
Ncesecrede <francescopm95@gmail.com>
2021-07-16 11:27:35 +0100

    Boolflix

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,5 +1,22 @@
 <template>
   <div class="header">
+      <div class="nav">
+          <ul class="nav-list-left">
+                <li>Logo</li>
+                <li><a href="#">Home</a></li>
+                <li><a href="#">Serie TV</a></li>
+                <li><a href="#">Film</a></li>
+                <li><a href="#">Originali</a></li>
+                <li><a href="#">Aggiunti di recente</a></li>
+                <li><a href="#">La mia lista</a></li>
+            </ul>
+            <ul class="nav-list-rigth">
+                <li>Search</li>
+                <li><a href="#">BAMBINI</a></li>
+                <li>Notifiche</li>
+                <li>Altro</li>
+            </ul>    
+      </div>
   </div>
 </template>
 
@@ -14,4 +31,42 @@ export default {
 </script>
 
 <style lang="scss">
+.header{
+    .nav{
+        height: 50px;
+        background-color: black;
+        display: flex;
+        justify-content: space-between;
+        color: lightgrey;
+        .nav-list-left{
+            display: flex;
+            list-style: none;
+            li{
+                margin: 0 5px;
+                a{
+                    color: lightgrey;
+                    text-decoration: none;
+                    &:hover{
+                        color: white;
+                    }
+                }
+            }
+        }
+        .nav-list-rigth{
+            display: flex;
+            list-style: none;
+            margin: 0 5px;
+            li{
+                margin: 0 5px;
+                    a{
+                        color: lightgrey;
+                        text-decoration: none;
+                        &:hover{
+                        color: white;
+                    }
+                }
+            }
+        }
+    }
+}
 </style>
