eb5a834a9bfc898b027a562d88cdfcabbf2555d0
Ncesecrede <francescopm95@gmail.com>
2021-07-16 10:37:29 +0100

    Boolflix

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,38 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import axios from "axios"
+import Header from "./components/Header.vue"
+import Main from "./components/Main.vue"
+
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Header,
+    Main
+  },
+  data (){
+    return {
+      movies: [],
+    }
+  },
+  created (){
+    axios.get("https://api.themoviedb.org/3/search/movie?api_key=3cd005812fa42de2c7826b6521d70f3f&query=ritorno+al+futuro").then((result) =>{
+      this.movies = result.data.response;
+    })
+  },
+  computed: {
+  },
+  methods: {
+
   }
 }
 </script>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+@import "./style/app.scss"
 </style>
---
+++ src/components/Main.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="main">
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'Main',
+  methods: {
+
+  }
+}
+</script>
+
+<style lang="scss">
+</style>
